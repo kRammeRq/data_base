e191d36b86b8e291704a0aeb94e6aa0bba8702cf
kRammeRq <167817535+kRammeRq@users.noreply.github.com>
2025-04-04 13:02:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 #include "database.h"
 #include <algorithm>
 #include "condition_parser.h"
-//#include <xutility>
 
 using namespace std;
 
@@ -86,27 +85,3 @@ int main() {
 
     return 0;
 }
-
-//void TestParseEvent() {
-//    {
-//        istringstream is("event");
-//        AssertEqual(ParseEvent(is), "event", "Parse event without leading spaces");
-//    }
-//    {
-//        istringstream is("   sport event ");
-//        AssertEqual(ParseEvent(is), "sport event ", "Parse event with leading spaces");
-//    }
-//    {
-//        istringstream is("  first event  \n  second event");
-//        vector<string> events;
-//        events.push_back(ParseEvent(is));
-//        events.push_back(ParseEvent(is));
-//        AssertEqual(events, vector<string>{"first event  ", "second event"}, "Parse multiple events");
-//    }
-//}
-//
-//void TestAll() {
-//    TestRunner tr;
-//    tr.RunTest(TestParseEvent, "TestParseEvent");
-//    tr.RunTest(TestParseCondition, "TestParseCondition");
-//}
